67299e294ddada33e80d0ae39cf00e5353ad07dd
Damqn <stu2101321082@uni-plovdiv.bg>
2023-03-07 14:44:58 +0200

    Added basic product service

---
+++ client/src/services/products.js
@@ -0,0 +1,17 @@
+import axios from "axios";
+const baseUrl = "http://localhost:3003/api/products"
+
+const getCategory = async (category) => {
+    const res = await axios.get(`${baseUrl}/${category}`)
+    return res.data;
+}
+
+const getProduct = async (id) => {
+    const res = await axios.get(`${baseUrl}/${id}`)
+    return res.data
+}
+
+export default {
+    getCategory,
+    getProduct
+}
