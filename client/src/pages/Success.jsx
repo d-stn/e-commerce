bc608bb82c39b34d3876699872ae11f32eb94f0f
Damqn <stu2101321082@uni-plovdiv.bg>
2023-03-22 18:17:38 +0200

    Added inventory management on order

--- client/src/pages/Success.jsx
+++ client/src/pages/Success.jsx
@@ -1,26 +1,70 @@
+import { Elements, useStripe } from "@stripe/react-stripe-js"
+import { loadStripe } from "@stripe/stripe-js"
 import { useEffect } from "react"
-import { useDispatch } from "react-redux"
+import { useDispatch, useSelector } from "react-redux"
 import { useNavigate } from "react-router-dom"
 import { clearCart } from "../reducers/cartReducer"
 
+import orderService from "../services/order"
+
+const stripePromise = loadStripe("pk_test_51MimZFJrvCD8ptIuFKxFW1lwgtrRG17zgGl0auVPkoFYE1lou7uCJytPgHaeGl5kF6D10uC39jroHWUhBQVOIeVV00MQewnjtY")
+
 const Success = () => {
+    return (
+        <Elements stripe={stripePromise}>
+            <SuccessElement />
+        </Elements>
+    )
+}
+
+const SuccessElement = () => {
     const navigate = useNavigate()
     const dispatch = useDispatch()
+    const stripe = useStripe()
+    const items = useSelector(state => state.cart.items)
 
     useEffect(() => {
-        const timeout = setTimeout(() => {
-            dispatch(clearCart())
-            navigate("/")
-        }, 5000);
+        if (!stripe) {
+            return
+        }
 
-        return () => clearTimeout(timeout)
-    }, [])
+        const clientSecret = new URLSearchParams(window.location.search).get(
+            "payment_intent_client_secret"
+        );
+
+        if (!clientSecret) {
+            return
+        }
+
+        const itemsToSend = items.map(e => {
+            return {
+                id: e.id,
+                qty: e.qty,
+                priceAtOrer: e.price
+            }
+        })
+
+        stripe
+            .retrievePaymentIntent(clientSecret)
+            .then(({ paymentIntent }) => {
+                const orderObjet = {
+                    address: paymentIntent.shipping.address,
+                    items: itemsToSend
+                }
+                orderService.createOrder(orderObjet)
+            })
+            .then(() => dispatch(clearCart()))
+    }, [stripe])
 
     return (
-        <div style={{ minHeight: "100vh" }}>
-            <h2>
-                Payment successfull. You are being redirected to the homepage. Please do not close the browser.
-            </h2>
+        <div className="success-container">
+            <div className="success">
+                <h1 className="title">
+                    Thank you for your purchase!
+                </h1>
+                <h3>Your items will be delivered between 3 - 8 business days</h3>
+                <button onClick={() => navigate("/")}>Continue shopping</button>
+            </div>
         </div>
     )
 }
